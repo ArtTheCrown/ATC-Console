9269a46153bcd982579b4e2a3ee220370280171b
Shumail Khan <raidenhellblade@gmail.com>
2024-06-08 20:30:10 +0100

    Feature: User model class updated.
    Description:
    - Added some variables for the user class
    
    Changes Made:
    - Added some variables for the user model class.
    
    Affected Files:
    - Models/User.cs
    
    Testing:
    - None
    
    Related Issues:
    - None
    
    Signed-off-by: Shumail Khan
    raidenhellblade@gmail.com

--- ATC-Console/Models/User.cs
+++ ATC-Console/Models/User.cs
@@ -2,6 +2,10 @@
 {
     public class User
     {
-
+        public static string Username { get; set; }
+        public static string Password { get; set; }
+        public static string Status { get; set; }
+        public static string Role { get; set; }
+        public static string Special { get; set; }
     }
 }
